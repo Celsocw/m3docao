901e7f62620af45b7c2f8a011e46d13c97778dde
mauro <73408974+holtzin29@users.noreply.github.com>
2025-11-26 20:21:56 -0300

    Update fcb.h

--- src/header/fcb.h
+++ src/header/fcb.h
@@ -1,1 +1,20 @@
+#ifndef FCB_H
+#define FCB_H
 
+
+#include <string>
+#include <map>
+#include <vector>
+#include <memory>
+#include <ctime>
+
+enum TipoDeArquivo {
+};
+
+
+using namespace std;
+
+class FCB {
+};
+
+#endif
