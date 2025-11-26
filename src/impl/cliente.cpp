8a773625a3eb7555e4054c8bbccc9f5b60b76fa9
mauro <73408974+holtzin29@users.noreply.github.com>
2025-11-26 20:55:54 -0300

    Update cliente.cpp

--- src/impl/cliente.cpp
+++ src/impl/cliente.cpp
@@ -1,1 +1,24 @@
 
+#include "../header/cliente.h"
+#include <iostream>
+
+using namespace std;
+
+void printHelp() {
+    cout << "\n=== COMANDOS DISPONIVEIS ===\n";
+    cout << "  mkdir <nome>            - Cria diretorio\n";
+    cout << "  cd <nome|..|/>          - Navega entre diretorios\n";
+    cout << "  ls                      - Lista arquivos com metadados\n";
+    cout << "  touch <nome> [tipo]     - Cria arquivo (tipo: text/num/bin/prog)\n";
+    cout << "  echo <arq> <conteudo>   - Escreve conteudo no arquivo\n";
+    cout << "  cat <arq>               - Le conteudo do arquivo\n";
+    cout << "  cp <origem> <destino>   - Copia arquivo\n";
+    cout << "  mv <origem> <destino>   - Move/renomeia arquivo\n";
+    cout << "  rm <nome>               - Remove arquivo ou diretorio\n";
+    cout << "  chmod <arq> <perm>      - Altera permissoes (ex: 755, 644)\n";
+    cout << "  stat <arq>              - Mostra metadados detalhados (inode, blocos)\n";
+    cout << "  su <uid> [gid]          - Troca usuario/grupo atual\n";
+    cout << "  whoami                  - Mostra usuario/grupo atual\n";
+    cout << "  help                    - Mostra esta ajuda\n";
+    cout << "  exit                    - Sai do simulador\n\n";
+}
