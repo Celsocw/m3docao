99926977f2a31c2f40801fb029a484c8f804b62a
mauro <73408974+holtzin29@users.noreply.github.com>
2025-11-26 20:20:44 -0300

    Update disco_virtual.h

--- src/header/disco_virtual.h
+++ src/header/disco_virtual.h
@@ -1,1 +1,16 @@
+#ifndef DISCO_VIRTUAL
+#define DISCO_VIRTUAL
 
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <stdexcept>
+#include "constants.h"
+
+using namespace std;
+class DiscoVirtual {
+
+}
+
+#endif
