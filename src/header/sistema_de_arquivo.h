b96a2ac366ae2386d84d5cb7665e63bac7e71e2d
mauro <73408974+holtzin29@users.noreply.github.com>
2025-11-26 20:23:25 -0300

    Update sistema_de_arquivo.h

--- src/header/sistema_de_arquivo.h
+++ src/header/sistema_de_arquivo.h
@@ -1,1 +1,18 @@
 
+#ifndef SISTEMA_ARQUIVO_H
+#define SISTEMA_ARQUIVO_H
+
+
+#include <memory>
+#include <string>
+#include "virtual_disk.h"
+#include "fcb.h"
+#include "constants.h"
+
+using namespace std;
+
+class SistemaArquivo {
+
+};
+
+#endif
